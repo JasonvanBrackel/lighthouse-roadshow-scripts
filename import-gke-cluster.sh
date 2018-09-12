9531b5288cdcc435e59b1ff3c1b2933377d39ebc
Jason L. van Brackel <jason@vanbrackel.net>
2018-09-12 16:35:29 -0400

    Add import-gke-cluster.sh

---
+++ import-gke-cluster.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Service Account
+serviceaccount="admin-user"
+
+kubectl -n kube-system create serviceaccount $serviceaccount
+
+# Cluster Role Binding
+cat <<EOF | kubectl create -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: $serviceaccount-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: $serviceaccount
+  namespace: kube-system
+EOF
+
+secret="$(kubectl -n kube-system get serviceaccount "$serviceaccount" -o 'jsonpath={.secrets[0].name}' 2>/dev/null)"
+
+# Context
+context="$(kubectl config current-context)"
+
+# Cluster
+cluster="$(kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.cluster}")"
+server="$(kubectl config view -o "jsonpath={.clusters[?(@.name==\"$cluster\")].cluster.server}")"
+
+# Token
+ca_crt_data="$(kubectl -n kube-system get secret "$secret" -o "jsonpath={.data.ca\.crt}" | openssl enc -d -base64 -A)"
+namespace="$(kubectl -n kube-system get secret "$secret" -o "jsonpath={.data.namespace}" | openssl enc -d -base64 -A)"
+token="$(kubectl -n kube-system get secret "$secret" -o "jsonpath={.data.token}" | openssl enc -d -base64 -A)"
+
+# Config
+export KUBECONFIG="$(mktemp)"
+kubectl config set-credentials "$serviceaccount" --token="$token" >/dev/null
+ca_crt="$(mktemp)"; echo "$ca_crt_data" > $ca_crt
+kubectl config set-cluster "$cluster" --server="$server" --certificate-authority="$ca_crt" --embed-certs >/dev/null
+kubectl config set-context "$context" --cluster="$cluster" --namespace="$namespace" --user="$serviceaccount" >/dev/null
+kubectl config use-context "$context" >/dev/null
+
+cat "$KUBECONFIG"
+
+# Rancher Import
+### INSERT RANCHER IMPORT COMMAND HERE
+
+
+rm $KUBECONFIG
+unset KUBECONFIG
+kubectl delete ClusterRoleBinding $serviceaccount-binding
+kubectl delete serviceaccount $serviceaccount
