c0fa4f7cb488cd8e96cd47c7c5229705a947bc57
Jason L. van Brackel <jason@vanbrackel.net>
2018-09-14 10:10:08 -0400

    Working version

--- replace-node-pools.sh
+++ replace-node-pools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cluster_name=c-qgrfw
-zone=us-central1-f
+cluster_name=<Your Cluster Name>
+zone=<Your Cluster Zone>
 old_nodepool=default-pool
 new_nodepool=marketplace-compatible
 
@@ -8,7 +8,7 @@ new_nodepool=marketplace-compatible
 gcloud container node-pools create $new_nodepool \
    --cluster $cluster_name --zone $zone \
    --num-nodes 3 \
-   --scopes=https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/service.management,https://www.googleapis.com/auth/trace.append
+   --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/devstorage.full_control,https://www.googleapis.com/auth/service.management,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring
 
 # Cordon off the old nodes
 kubectl cordon -l cloud.google.com/gke-nodepool=$old_nodepool
